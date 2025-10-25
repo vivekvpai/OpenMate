6831e014a723d2395bfeb639d01d01c4aa907852
vivekvpai <paivivek002@gmail.com>
2025-10-25 17:34:13 +0530

    updated

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/docs/',
+  base: '/OpenMate/',
   plugins: [react()],
   publicDir: 'public',
   resolve: {
